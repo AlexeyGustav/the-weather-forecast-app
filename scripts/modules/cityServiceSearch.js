551d641fc5cd375a1824d8c61dd5cf8b2355fbd8
Alexey <gustavaleksej9@gmail.com>
2023-07-20 18:31:42 +0300

    prodaction

--- scripts/modules/cityServiceSearch.js
+++ scripts/modules/cityServiceSearch.js
@@ -1,31 +1,38 @@
 import { startWidget } from "./widgetService.js";
 
+
 export const cityServiceSearch = (widget) => {
-  const button = document.querySelector('.widget__change-city');
+
+  setTimeout(() => {
+
+  
+    const button = document.querySelector('.widget__change-city');
 
 
-  button.addEventListener('click', () => {
-    const form = document.createElement('form');
-    form.classList.add('widget__form');
+    button.addEventListener('click', () => {
+      const form = document.createElement('form');
+      form.classList.add('widget__form');
 
-    const inputCity = document.createElement('input');
-    inputCity.classList.add('widget__input');
-    inputCity.name = 'city';
-    inputCity.type = 'search';
-    inputCity.placeholder = 'Введите город';
+      const inputCity = document.createElement('input');
+      inputCity.classList.add('widget__input');
+      inputCity.name = 'city';
+      inputCity.type = 'search';
+      inputCity.placeholder = 'Введите город';
 
-    form.append(inputCity);
-    widget.append(form);
+      form.append(inputCity);
+      widget.append(form);
+      
 
-    inputCity.focus();
+      inputCity.focus();
     
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault(); 
+      form.addEventListener('submit', async (e) => {
+        e.preventDefault(); 
 
-      widget.textContent = '';
-      await startWidget(inputCity.value, widget);
+        widget.textContent = '';
+        await startWidget(inputCity.value, widget);
 
-      cityServiceSearch(widget)
+        cityServiceSearch(widget)
+      });
     });
-  });
+  },1000)
 };
